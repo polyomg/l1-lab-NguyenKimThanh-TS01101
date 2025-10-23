25f6e9448735359b455bcc3409de89c03d92e44f
Nguyen Kim Thanh <tenemailcuamay@gmail.com>
2025-10-23 22:03:52 +0700

    Lab7-TS01101-Java5

---
+++ Lab7/src/main/java/poly/edu/Lab7Ts01101Java5Application.java
@@ -0,0 +1,13 @@
+package poly.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab7Ts01101Java5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab7Ts01101Java5Application.class, args);
+	}
+
+}
